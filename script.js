3898b93a5221ef849e5a605c4c5e0a955a35f1b1
christopher8126 <christopher.salvador8126@gmail.com>
2022-04-04 10:43:24 +0800

    adding css displays

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.body;
 const homepage = document.querySelector('.homepage');
 const benefits = document.querySelector('.benefits');
+const about = document.querySelector('.about');
 
 const loadAnim = () => {
   lottie.loadAnimation({
@@ -9,6 +10,20 @@ const loadAnim = () => {
     loop: true,
     autoplay: true,
     path: '/assets/wave-files/data-color-1.json',
+  });
+  lottie.loadAnimation({
+    container: benefits,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/assets/wave-files/data-color-2.json',
+  });
+  lottie.loadAnimation({
+    container: about,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/assets/wave-files/data-color-3.json',
   });
 };
 
