e927a7ce197872a87242e9e0de96b60bc7b61784
christopher8126 <christopher.salvador8126@gmail.com>
2022-04-05 17:00:44 +0800

    done sass

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const body = document.body;
 const homepage = document.querySelector('.homepage');
 const benefits = document.querySelector('.benefits');
 const about = document.querySelector('.about');
+const satisfaction = document.querySelector('.satisfaction');
+const work = document.querySelector('.work');
+const products = document.querySelector('.products');
+const plant = document.querySelector('.plant');
+const backToTop = document.querySelector('.bottombtn');
 
 const loadAnim = () => {
   lottie.loadAnimation({
@@ -23,8 +28,39 @@ const loadAnim = () => {
     renderer: 'svg',
     loop: true,
     autoplay: true,
+    path: '/assets/wave-files/data-color-2.json',
+  });
+  lottie.loadAnimation({
+    container: satisfaction,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/assets/wave-files/data-color-2.json',
+  });
+  lottie.loadAnimation({
+    container: work,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/assets/wave-files/data-color-3.json',
+  });
+  lottie.loadAnimation({
+    container: products,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/assets/wave-files/data-color-2.json',
+  });
+  lottie.loadAnimation({
+    container: plant,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
     path: '/assets/wave-files/data-color-1.json',
   });
 };
 
 body.addEventListener('load', loadAnim());
+backToTop.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
